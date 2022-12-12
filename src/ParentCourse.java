8ef07e32de6243d0a90d2dcf014e034eb3ddb2a3
MazingerDev <mazinasd7@gmail.com>
2022-12-12 16:18:38 +0200

    update .gitignore

---
+++ src/ParentCourse.java
@@ -0,0 +1,10 @@
+package PACKAGE_NAME;import java.util.*;
+
+public class ParentCourse {
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        
+    }
+}
