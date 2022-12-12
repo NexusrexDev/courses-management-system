56e44822773491cb543d3c25be46163164ab0c48
Nexusrex <ahmedtahoun55@gmail.com>
2022-12-12 23:05:10 +0200

    Created globals class, includes paths to every directory to reference instead of using strings.

---
+++ src/Global.java
@@ -0,0 +1,11 @@
+public final class Global {
+    public static final String AdminLogin = "Login/Admin.txt";
+    public static final String InstructorLogin = "Login/Instructors.txt";
+    public static final String StudentLogin = "Login/Students.txt";
+    public static final String StudentFolder = "Students/";
+    public static final String InstructorFolder = "Instructors/";
+    public static final String CourseFolder = "Courses/";
+    public static final String ParentCourseFolder = "ParentCourses/";
+    public static final String ReportFolder = "Reports/";
+    public static final String SurveyFolder = "Survey/";
+}
